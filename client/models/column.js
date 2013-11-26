fd01e0b5623e720b0ebadeb838c43b7f24b8fdab
sergeyt <stodyshev@gmail.com>
2013-11-26 10:12:56 +0700

    fetching work items from fogbugz

--- client/models/column.js
+++ client/models/column.js
@@ -1,5 +1,6 @@
 // get column items
 Template.column.items = function(){
     var items = WorkItems.find({status: this.status}, {}).fetch();
+    console.log('fetched %d items', items.length);
     return items;
 };
--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,10 +1,10 @@
-// TODO init from fogbugz
+var Future = Npm.require("fibers/future");
 
 function resolveCaseStatus(c){
     var s = c.status.name.toLowerCase();
-    if (s.indexOf('review')) return 'review';
-    if (s.indexOf('resolved')) return 'test';
-    if (s.indexOf('close')) return 'done';
+    if (s.indexOf('review') >= 0) return 'review';
+    if (s.indexOf('resolved')  >= 0) return 'test';
+    if (s.indexOf('close')  >= 0) return 'done';
     return 'active';
 }
 
@@ -14,6 +14,7 @@ function caseToWorkItem(c){
         id: c.id,
         title: c.title,
         status: resolveCaseStatus(c),
+        board: 'AR8',
         // TODO provide dates in fogbugz.js
         created: timestamp,
         modified: timestamp
@@ -40,6 +41,11 @@ Meteor.startup(function () {
     });
 
     // fetch data from fogbugz
+    console.log('connecting to fogbugz %s under %s', Meteor.settings.fogbugz, Meteor.settings.user);
+
+    var itemsFuture = new Future();
+    var itemsResolver = itemsFuture.resolver();
+
     FOGBUGZ.connect({
         url: Meteor.settings.fogbugz,
         user: Meteor.settings.user,
@@ -48,10 +54,17 @@ Meteor.startup(function () {
             return fb.search();
     }).then(function(list){
         console.log('fetched %d cases', list.length);
-        list.map(caseToWorkItem).forEach(function(it){
-            WorkItems.insert(it);
-        });
-        console.log('db updated!');
+        itemsResolver(null, list.map(caseToWorkItem));
+    }).fail(function(error){
+        console.log('error: %s', error);
+        itemsResolver([]);
     });
 
+    var items = itemsFuture.wait();
+    for (var i = 0; i < items.length; i++){
+        var it = items[i];
+        WorkItems.insert(it);
+        console.log('inserted %s: %s', it.id, it.title);
+    }
+    console.log('db updated!');
 });
