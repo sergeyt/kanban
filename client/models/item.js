c872e228e7b29ac1772cf22eae641defbf52aa15
sergeyt <stodyshev@gmail.com>
2013-11-27 21:35:39 +0700

    goal indicator

--- client/models/item.js
+++ client/models/item.js
@@ -2,59 +2,63 @@
 var workItemsHandle = null;
 
 Deps.autorun(function() {
-  var board = Session.get('board');
-  if (board) {
-	workItemsHandle = Meteor.subscribe('workItems', board);
-  } else {
-	workItemsHandle = null;
-  }
+	var board = Session.get('board');
+	if (board) {
+		workItemsHandle = Meteor.subscribe('workItems', board);
+	} else {
+		workItemsHandle = null;
+	}
 });
 
 Template.items.loading = function() {
-  return !(workItemsHandle && workItemsHandle.ready());
+	return !(workItemsHandle && workItemsHandle.ready());
 };
 
 Template.item.helpers({
-  assigneeShortName: function() {
-	return shortName(this.assignee);
-  },
+	goalClass: function() {
+		return (this.tags || []).indexOf('goal') >= 0 ? 'goal' : 'hidden';
+	},
 
-  avatarUrl: function() {
-	return Gravatar.imageUrl(this.assignee.email, {s: 20});
-  },
+	assigneeShortName: function() {
+		return shortName(this.assignee);
+	},
 
-  assigneeState: function() {
-	return shortName(this.assignee) == 'CLOSED' ? 'hidden' : '';
-  }
+	avatarUrl: function() {
+		return Gravatar.imageUrl(this.assignee.email, {s: 20});
+	},
+
+	assigneeState: function() {
+		return shortName(this.assignee) == 'CLOSED' ? 'hidden' : '';
+	}
 });
 
 Template.item.rendered = function() {
-  var id = this.data._id;
-  $(this.firstNode).draggable({
-	cursor: "move",
-	opacity: 0.7,
-	helper: "clone",
-	start: function() {
-	  Session.set('dragItem', id);
-	}
-  });
+	var id = this.data._id;
+	$(this.firstNode).draggable({
+		cursor: "move",
+		opacity: 0.7,
+		helper: "clone",
+		start: function() {
+			Session.set('dragItem', id);
+		}
+	});
 };
 
 function shortName(user) {
-  if (!user || !user.name) {
-	return '';
-  }
-  var arr = user.name.split(' ');
-  if (arr[0].length <= 3) {
-	return arr.slice(0, 2).join(' ');
-  }
-  return arr.length <= 1 ? user.name : abbr(arr[0]) + arr[1].substr(0, 1);
+	if (!user || !user.name) {
+		return '';
+	}
+	var arr = user.name.split(' ');
+	if (arr[0].length <= 3) {
+		return arr.slice(0, 2).join(' ');
+	}
+	return arr.length <= 1 ? user.name : abbr(arr[0]) + arr[1].substr(0, 1);
 }
 
 function abbr(name) {
-  switch (name.toLowerCase()) {
-	case 'alexander':
-	  return 'Alex';
-  }
-  return name;
+	switch (name.toLowerCase()) {
+		case 'alexander':
+			return 'Alex';
+	}
+	return name;
 }
