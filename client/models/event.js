74502019d77796e8f6261cf6726a1987f8c5f279
sergeyt <stodyshev@gmail.com>
2013-11-28 10:35:33 +0700

    display event date in relative format

--- client/models/event.js
+++ client/models/event.js
@@ -1,6 +1,9 @@
 // work item event
 
 Template.event.helpers({
+  when: function(){
+	return PrettyDate.format(this.date);
+  },
   bodyClass: function(){
 	return this.html || this.text ? '' : 'hidden';
   },
