eb8efba71dfeae26699e3fe504a75cd40563b06e
sergeyt <stodyshev@gmail.com>
2013-11-29 21:11:00 +0700

    updated legend (goal, percentage of done)

--- client/models/legend.js
+++ client/models/legend.js
@@ -2,18 +2,40 @@ function calcCategoryPercentage(category) {
 	return (WorkItems.find({category: category}).count() / WorkItems.find().count() * 100).toFixed(2);
 }
 
+function calcDoneCategoryPercentage(category) {
+	return (WorkItems.find({category: category, status: 'done'}).count() / WorkItems.find().count() * 100).toFixed(2);
+}
+
 Template.legend.helpers({
+	goal: function() {
+		return (WorkItems.find({tags: 'goal'}).count() / WorkItems.find().count() * 100).toFixed(2);
+	},
+	goalDone: function() {
+		return (WorkItems.find({tags: 'goal', status: 'done'}).count() / WorkItems.find().count() * 100).toFixed(2);
+	},
 	bug: function() {
 		return calcCategoryPercentage('bug');
+	},
+	bugDone: function() {
+		return calcDoneCategoryPercentage('bug');
 	},
 	feature: function() {
 		return calcCategoryPercentage('feature');
 	},
+	featureDone: function() {
+		return calcDoneCategoryPercentage('feature');
+	},
 	task: function() {
 		return calcCategoryPercentage('task');
 	},
+	taskDone: function() {
+		return calcDoneCategoryPercentage('task');
+	},
 	inquiry: function() {
 		return calcCategoryPercentage('inquiry');
+	},
+	inquiryDone: function() {
+		return calcDoneCategoryPercentage('inquiry');
 	}
 });
 
