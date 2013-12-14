db2e846d12072bf44edc16279ba7ad37b3b3c118
sergeyt <stodyshev@gmail.com>
2013-12-14 21:52:30 +0700

    revised theme selector

--- client/themes.js
+++ client/themes.js
@@ -1,16 +1,26 @@
 var themes = [
 	{name: 'default', label: 'Default'},
+	{name: 'amelia', label: 'Amelia'},
+	{name: 'cerulean', label: 'Cerulean'},
+	{name: 'cosmo', label: 'Cosmo'},
 	{name: 'cyborg', label: 'Cyborg'},
+	{name: 'flatly', label: 'Flatly'},
+	{name: 'journal', label: 'Journal'},
+	{name: 'readable', label: 'Readable'},
+	{name: 'simplex', label: 'Simplex'},
 	{name: 'slate', label: 'Slate'},
+	{name: 'spacelab', label: 'Spacelab'},
 	{name: 'united', label: 'United'},
 	{name: 'yeti', label: 'Yeti'}
 ];
 
-themes.forEach(function(theme){
+Template.themes.list = themes.map(function(theme){
 	var name = theme.name;
-	Template.themes[name] = function(){
-		return UserSession.get('theme') == name;
-	};
+	return _.extend({}, theme, {
+		selected: function(){
+			return UserSession.get('theme') == name;
+		}
+	});
 });
 
 Template.themeSelector.themes = function(){
