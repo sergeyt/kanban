85a2138e1f7a8e8bfc3b857647af27ccc36fa9b6
sergeyt <stodyshev@gmail.com>
2013-12-16 14:08:08 +0700

    deep update

--- update.cmd
+++ update.cmd
@@ -1,2 +1,11 @@
 git submodule update --init
 git submodule foreach git pull origin master
+
+cd app\packages\async
+git submodule update --init
+cd ..\..\..
+
+cd app\packages\moment
+git submodule update --init
+cd ..\..\..
+
