e0777c4b229cf4d7381f64490a73bb3d9d0100ba
sergeyt <stodyshev@gmail.com>
2013-12-24 01:05:15 +0700

    working script
    This reverts commit 294a6cea0076190d390f3d365f10ef3e6028f37f.

--- meteor.sh
+++ meteor.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make
+sudo apt-get install python-software-properties
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+
+# install mongodb and base tools
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee -a /etc/apt/sources.list.d/10gen.list
 sudo apt-get update
-
-# install mongodb
 sudo apt-get install -y git mongodb-10gen curl
 
-# install nodejs from tarball
-# cd /usr/local
-# wget http://nodejs.org/dist/v0.8.23/node-v0.8.23-linux-x86.tar.gz
-# sudo tar -xvzf node-v0.8.23-linux-x86.tar.gz --strip=1
-# rm -f node-v0.8.23-linux-x86.tar.gz
-
-# install nodejs using apt-get
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs
-
-# install npm packages
-sudo npm install -g grunt-cli
-sudo npm install -g karma
-sudo npm install -g mocha
-sudo npm install -g bower
+# install nodejs
+cd /usr/local
+wget http://nodejs.org/dist/v0.8.23/node-v0.8.23-linux-x86.tar.gz
+sudo tar -xvzf node-v0.8.23-linux-x86.tar.gz --strip=1
+rm -f node-v0.8.23-linux-x86.tar.gz
 
 # install meteor and meteorite
 curl https://install.meteor.com | sudo sh
