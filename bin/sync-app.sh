33fd3050ad4c5218b31a53fb6b3af014c9d4c8ff
sergeyt <stodyshev@gmail.com>
2014-01-17 16:30:33 +0700

    updated vagrant scripts

--- bin/sync-app.sh
+++ bin/sync-app.sh
@@ -5,9 +5,7 @@ sudo rsync -rtvu --progress /vagrant/app/ /home/vagrant/app/
 
 EVENTS="CREATE,CLOSE_WRITE,DELETE,MODIFY,MOVED_FROM,MOVED_TO"
 
-inotifywait -mrq -e $EVENTS --format '%w' /vagrant/app/ | while read MODFILE
-do
-  echo need to rsync '$MODFILE' ...
-  $TARGET_FILE = /home$MODFILE
-  sudo rsync -av $MODFILE $TARGET_FILE
+while true
+  do inotifywait -mrq -e $EVENTS --format '%w' /vagrant/app/
+  sudo rsync -rtvu --progress /vagrant/app/ /home/vagrant/app/
 done
