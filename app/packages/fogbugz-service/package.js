08a475d3b7c9b3ac169d277300223066c4d63088
sergeyt <stodyshev@gmail.com>
2014-01-19 23:48:01 +0700

    using latest fogbugz.js

--- app/packages/fogbugz-service/package.js
+++ app/packages/fogbugz-service/package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "FogBugz service for kanban"
 });
 
-Npm.depends({"fogbugz.js": "0.0.26"});
+Npm.depends({"fogbugz.js": "0.0.30"});
 
 Package.on_use(function (api) {
 	api.use('underscore', ['server']);
